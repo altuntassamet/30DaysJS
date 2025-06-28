3949e7866a086c236d73a79f340412d9e74a6e20
Samet Altuntaş <99142668+altuntassamet@users.noreply.github.com>
2025-06-28 18:56:13 +0300

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,43 @@
+//consola yazı yazma
+console.log ('Hello world')
+
+// Toplama işleminin sonucunu consola basma
+console.log (16+2)
+
+// Çıkartma işleminin sonucunu consola basma
+console.log (16-2)
+
+// Çarpma işleminin sonucunu consola basma
+console.log (16*2)
+
+// Bölme işleminin sonucunu consola basma
+console.log (16/2)
+
+// Bölme işleminden kalan sonucu consola basma
+console.log (16%2)
+
+// Üs alma işleminin sonucunu consola basma
+console.log (16**2)
+
+/*Veri türleri
+Numbers ==> -16, -7.5, -2, 0, 4.3, 16 gibi
+Strings ==> "", '', yada `` içerisindeki birden fazla karakter
+Booleans ==> true yada false
+Undefined ==> değer atanmamış
+Null ==> boş değer   */
+
+console.log (typeof 16)
+console.log (typeof 'Samet')
+console.log (typeof true)
+console.log (typeof Samet)
+console.log (typeof null)
+
+//Variables/Değişkenler
+// Let ==> Değiştirilebilir
+// Cons ==> Değiştirilemez
+
+const name = "Samet",
+    age = 34,
+    city = "Kocaeli",
+    live = true
+console.log(name, age, city, live)
