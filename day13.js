1e79b61a066710cab616b46f00a065557f083de6
Samet Altuntaş <99142668+altuntassamet@users.noreply.github.com>
2025-07-11 17:10:19 +0300

    Add files via upload

---
+++ day13.js
@@ -0,0 +1,12 @@
+//console methods
+console.log('%cDUR', 'font-size: 150px; color: red')
+
+console.warn('uyarı mesajı')
+console.error('hata mesajı')
+
+let a = 5
+let b = 15
+// assert ==> iştem tru ise hiç bir şey olmaz. false ise mesajı çalıştırır.
+console.assert(a > b, 'a, b den büyük değildir.')
+
+console.clear()
